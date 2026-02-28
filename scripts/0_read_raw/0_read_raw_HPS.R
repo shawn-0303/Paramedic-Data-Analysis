52965cd8fa33e11db2985b38de23d54b24c2e6b9
Shawn Yip <shawniceyip@trentu.ca>
2026-02-28 21:31:42 +0000

    Read in HPS pickup lat and long

--- scripts/0_read_raw/0_read_raw_HPS.R
+++ scripts/0_read_raw/0_read_raw_HPS.R
@@ -4,5 +4,14 @@ library(here)
 raw_HPS <- read_xlsx(here("data", "raw_data" , "HPS-InTox_Data_Pull.xlsx"),
                      col_names = TRUE)
 
+# Pick up location lat and long added later
+raw_HPS_pickup_locations <- read_xlsx(here("data", "raw_data" , "HPS-Lat_long.xlsx"),
+                                      col_names = TRUE)
+
+# join datasets
+raw_HPS <- raw_HPS |>
+           inner_join(raw_HPS_pickup_locations,
+                      by = "Call Number/Patient Number")
+
 save(raw_HPS,
      file = here("data", "raw_data", "raw_HPS.rda"))
